d9ad35799ff383751df850cffd42b5fe494ddbd5
Somenath Das <bikramdas76@gmail.com>
2019-02-03 22:07:57 +0530

    Ignored file commits

---
+++ precache-manifest.4bef26ff942d078c4ce9f07af9a2cdfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cec0cfcceeed2ddfff03",
+    "url": "/static/js/main.cec0cfcc.chunk.js"
+  },
+  {
+    "revision": "0ed7bc6869562f7b598e",
+    "url": "/static/js/1.0ed7bc68.chunk.js"
+  },
+  {
+    "revision": "cec0cfcceeed2ddfff03",
+    "url": "/static/css/main.6a1712dd.chunk.css"
+  },
+  {
+    "revision": "0ed7bc6869562f7b598e",
+    "url": "/static/css/1.705c3042.chunk.css"
+  },
+  {
+    "revision": "cec2aba5d33452f07397850d0e8ab0b5",
+    "url": "/index.html"
+  }
+];
