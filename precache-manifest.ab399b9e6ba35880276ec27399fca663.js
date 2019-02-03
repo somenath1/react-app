5c87768a7be6eb58889d2b61d061beba7bf07f1f
Somenath Das <bikramdas76@gmail.com>
2019-02-03 21:45:01 +0530

    Image added

---
+++ precache-manifest.ab399b9e6ba35880276ec27399fca663.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "71604c2e4e4da671c507",
+    "url": "/static/js/main.71604c2e.chunk.js"
+  },
+  {
+    "revision": "0ed7bc6869562f7b598e",
+    "url": "/static/js/1.0ed7bc68.chunk.js"
+  },
+  {
+    "revision": "71604c2e4e4da671c507",
+    "url": "/static/css/main.e4b4dfad.chunk.css"
+  },
+  {
+    "revision": "0ed7bc6869562f7b598e",
+    "url": "/static/css/1.705c3042.chunk.css"
+  },
+  {
+    "revision": "3324bcb50da62f73c16664bc9c4c3a66",
+    "url": "/index.html"
+  }
+];
